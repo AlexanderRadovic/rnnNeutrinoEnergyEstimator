7f702fbb4bf4711d1a092d9c82077b0f804d0618
Alexander <a.radovic@gmail.com>
2017-03-28 14:42:07 -0500

    Cleaned up the script, added comments.

--- nova_regression_mva_numu_multiinput.py
+++ nova_regression_mva_numu_multiinput.py
@@ -33,22 +33,19 @@ class colors:
     close = '\033[0m'
 
 
-max_features = 20000
-maxlen = 10  # cut texts after this number of words (among top max_features most common words)
 batch_size = 32
 
 print('Loading data...')
-X = np.genfromtxt('numu/inputList.txt',delimiter='*',dtype='string')
-Y = np.genfromtxt('numu/truthList.txt')
-H = np.genfromtxt('numu/remainderList.txt')
+X = np.genfromtxt('numu/inputList.txt',delimiter='*',dtype='string') #the prong level info
+Y = np.genfromtxt('numu/truthList.txt') #the labels
+H = np.genfromtxt('numu/remainderList.txt') #the "header" with event level information
 
-#H=np.concatenate((H, H))
-
+#dimensions of the prong level information
 number_of_variables = 12
 number_of_prongs = 10
 
+#reformat prong level input to be broken down by prong and variable.
 X_mva=np.zeros((len(X),len(X[0]),number_of_variables))
-
 for i in range(0,len(X)):
     for j in range(0,number_of_prongs):
         X_mva[i][j]=(X[i][j].split(","))
@@ -68,9 +65,9 @@ print(Y[0], 'first entry, train')
 print(len(H), 'target sequences')
 print(H[0], 'first entry, train')
 
-#X = sequence.pad_sequences(X, maxlen=maxlen)
 print(X[0], 'first entry, train')
 
+#split into train and test samples
 X_test=X_mva[int(X_mva.shape[0]*0.8):];
 Y_test=Y[int(Y.shape[0]*0.8):];
 H_test=H[int(H.shape[0]*0.8):];
@@ -79,10 +76,10 @@ X_train=X_mva[:int(X_mva.shape[0]*0.8)];
 Y_train=Y[:int(Y.shape[0]*0.8)];
 H_train=H[:int(H.shape[0]*0.8)];
 
-Y_train = np.reshape(Y_train, len(Y_train))
+#reshape the header so that it's 2d rather than 1d, which is what keras expects
+#Y_train = np.reshape(Y_train, len(Y_train))
 H_train = np.reshape(H_train, (len(H_train),1))
-
-Y_train = np.reshape(Y_train, len(Y_train))
+#Y_train = np.reshape(Y_train, len(Y_train))
 H_test = np.reshape(H_test, (len(H_test),1))
 
 print('X_train shape:', X_train.shape)
@@ -98,34 +95,42 @@ print(X_train[0], 'first entry, train')
 print(Y_train[0], 'first entry, train')
 print(H_train[0], 'first entry, train')
 
-print('Build model...')
 print(H_train[0].shape)
 print(Y_train[0].shape)
 print(X_train[0].shape)
 
+print('Build model...')
+
+#define a model with multiple inputs, one based on prongs the other using our event level header
 main_input = Input(shape=X_train[0].shape, dtype='float', name='main_input')
 aux_input = Input(shape=H_train[0].shape, dtype='float', name='aux_input')
 
+#push the prong level information through a LSTM
 main_branch=LSTM(16)(main_input)
 
+#merge the prong and header level information
 x = keras.layers.merge([main_branch, aux_input], mode='concat')
 
+#use combined output to make our energy estimate
 main_output = Dense(1, activation='linear', name='main_output')(x)
 
 model = Model(input=[main_input, aux_input], output=main_output)
 
-model.compile(loss='mse',#loss='binary_crossentropy',
+#mse as a regression task, rmsprop is meant to be be a good pick of LSTMs
+model.compile(loss='mse',
               optimizer='rmsprop')
 
 print('Train...')
-start_time = time.time()
-epochs = 2
 
+#start the training, set number of epochs
+start_time = time.time()
+epochs = 3
 resultLog=model.fit([X_train,H_train], Y_train, batch_size=batch_size, nb_epoch=epochs,
           validation_data=([X_test,H_test], Y_test))
 
 average_time_per_epoch = (time.time() - start_time) / epochs    
 
+#take a look at final performance on some example events from the test set
 for i in range(10):
     ind = np.random.randint(0, len(X_test))
     rowX, rowH, rowy = X_test[np.array([ind])], H_test[np.array([ind])], Y_test[np.array([ind])]
@@ -146,7 +151,6 @@ for i in range(10):
 
 
 # Compare models' accuracy, loss and elapsed time per epoch.
-
 print(average_time_per_epoch,'average time per epoch')
 
 # plt.style.use('ggplot')
@@ -160,6 +164,9 @@ print(average_time_per_epoch,'average time per epoch')
 # plt.show()
 
 print(resultLog.history)
+
+#save the model definition + weights
 model.save('my_model.hdf5')
+#save just the weightsx
 model.save_weights('test.hdf5') 
 
