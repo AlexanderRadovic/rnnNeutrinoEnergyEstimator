a6a0a39254807464f30254faed904390d0ddbf94
Alexander <a.radovic@gmail.com>
2017-03-28 14:45:05 -0500

    Changed opening script descriptor to be more accurate.

--- nova_regression_mva_numu_multiinput.py
+++ nova_regression_mva_numu_multiinput.py
@@ -1,15 +1,7 @@
 # -*- coding: utf-8 -*-
-'''Trains a LSTM on the IMDB sentiment classification task.
-The dataset is actually too small for LSTM to be of any advantage
-compared to simpler, much faster methods such as TF-IDF + LogReg.
-Notes:
-
-- RNNs are tricky. Choice of batch size is important,
-choice of loss and optimizer is critical, etc.
-Some configurations won't converge.
-
-- LSTM loss decrease patterns during training can be quite different
-from what you see with CNNs/MLPs/etc.
+'''
+An attempt to build a LSTM based network for neutrino energy information. 
+Combining reconstructed object level informaton with event level information.
 '''
 from __future__ import print_function
 import numpy as np
