89e2957e57668bef965e6e157ca4155dedd88cc8
AlexanderRadovic <a.radovic@gmail.com>
2018-03-22 15:05:17 -0500

    Some spring cleaning and comment adding/editing

--- nova_regression_mva_numu_multiinput_multioutput.py
+++ nova_regression_mva_numu_multiinput_multioutput.py
@@ -2,6 +2,8 @@
 '''
 An attempt to build a LSTM based network for neutrino energy information. 
 Combining reconstructed object level informaton with event level information.
+The network is also designed to be multitarget, with the aim of also predicting
+other useful kinematic information.
 '''
 from __future__ import print_function
 
@@ -26,6 +28,7 @@ from keras.callbacks import LearningRateScheduler
 
 import newLossFunctions
 
+#define a learning rate strategy
 def learning_rate_plan(epoch):
     learningRate=0.001
     if epoch<21:
@@ -35,15 +38,6 @@ def learning_rate_plan(epoch):
     elif epoch>=41:
          learningRate=learningRate*0.25
 
-    # if epoch<21:
-    #     learningRate=learningRate*1
-    # elif epoch>=21 and epoch<41:
-    #      learningRate=learningRate*0.5
-    # elif epoch>=41 and epoch<61:
-    #      learningRate=learningRate*0.25
-    # elif epoch>=61:
-    #      learningRate=learningRate*0.125
-         
     return learningRate
 
 class colors:
@@ -67,28 +61,22 @@ X_mva=np.zeros((len(X),len(X[0]),number_of_variables))
 for i in range(0,len(X)):
     for j in range(0,number_of_prongs):
         X_mva[i][j]=(X[i][j].split(","))
-        #X_mva[i][j]=(X[i][number_of_prongs-(j+1)].split(","))
 
 print('Y_raw shape:', Y_raw.shape)
-#reformat prong level input to be broken down by prong and variable.
+
+#extract target information
 Y=np.zeros(len(X))
+Y_lept=np.zeros(len(X))
+Y_had=np.zeros(len(X))
+Y_hadfrac=np.zeros(len(X))
+
 for i in range(0,len(X)):
         Y[i]=float(Y_raw[i][0])
-
-Y_lept=np.zeros(len(X))
-for i in range(0,len(X)):
         Y_lept[i]=float(Y_raw[i][1])
-
-Y_had=np.zeros(len(X))
-for i in range(0,len(X)):
         Y_had[i]=float(Y_raw[i][2])
-
-Y_hadfrac=np.zeros(len(X))
-for i in range(0,len(X)):
         Y_hadfrac[i]=float(Y_raw[i][3])
 
-
-        
+#check dimensions and sample inputs match expectations        
 print('X shape:', X.shape)
 print('X mva shape:', X_mva.shape)
 print('Y_raw shape:', Y_raw.shape)
@@ -107,7 +95,7 @@ print(H[0], 'first entry, train')
 
 print(X[0], 'first entry, train')
 
-#split into train and test samples
+#split into train and test samples, use indices to keep various arrays in sync
 indices = np.arange(X_mva.shape[0])
 np.random.shuffle(indices)
 
@@ -126,37 +114,20 @@ Y_had_train=Y_had[indices[:int(Y.shape[0]*0.8)]];
 Y_hadfrac_train=Y_hadfrac[indices[:int(Y.shape[0]*0.8)]];
 H_train=H[indices[:int(H.shape[0]*0.8)]];
 
-# X_test=X_mva[int(X_mva.shape[0]*0.8):];
-# Y_test=Y[int(Y.shape[0]*0.8):];
-# H_test=H[int(H.shape[0]*0.8):];
-
-# X_train=X_mva[:int(X_mva.shape[0]*0.8)];
-# Y_train=Y[:int(Y.shape[0]*0.8)];
-# H_train=H[:int(H.shape[0]*0.8)];
-
-
 #reshape the header so that it's 2d rather than 1d, which is what keras expects
-#Y_train = np.reshape(Y_train, len(Y_train))
 H_train = np.reshape(H_train, (len(H_train),1))
-#Y_train = np.reshape(Y_train, len(Y_train))
 H_test = np.reshape(H_test, (len(H_test),1))
 
 print('X_train shape:', X_train.shape)
 print('X_test shape:', X_test.shape)
-
 print('Y_train shape:', Y_train.shape)
 print('Y_test shape:', Y_test.shape)
-
 print('H_train shape:', H_train.shape)
 print('H_test shape:', H_test.shape)
 
 print(X_train[0], 'first entry, train')
 print(Y_train[0], 'first entry, train')
 print(H_train[0], 'first entry, train')
-
-print(H_train[0].shape)
-print(Y_train[0].shape)
-print(X_train[0].shape)
 
 print('Build model...')
 
@@ -165,15 +136,12 @@ main_input = Input(shape=X_train[0].shape, dtype='float', name='main_input')
 aux_input = Input(shape=H_train[0].shape, dtype='float', name='aux_input')
 
 #push the prong level information through a LSTM or some other form of RNN
-#main_branch=TimeDistributed(Dense(32))(main_input)
-#main_branch=TimeDistributed(Dense(16))(main_input)
+
 main_branch=LSTM(16)(main_input)#,W_regularizer = l1(0.001),U_regularizer = l1(0.001),b_regularizer = l1(0.001))(main_input)
 
 #merge the prong and header level information
 x = keras.layers.merge([main_branch, aux_input], mode='concat')
-#x=Dense(16)(x)
-#x=Dense(16)(x)
-#x=Dense(16)(x)
+
 #use combined output to make our energy estimate
 main_output = Dense(1, activation='linear', name='main_output')(x)
 lepton_output = Dense(1, activation='linear', name='lepton_output')(x)
@@ -182,7 +150,7 @@ hadfrac_output = Dense(1, activation='linear', name='hadfrac_output')(x)
 
 model = Model(input=[main_input, aux_input], output=[main_output,lepton_output,hadron_output,hadfrac_output])
 
-#mse as a regression task, rmsprop is meant to be be a good pick of LSTMs
+#mse as a regression task, rmsprop is meant to be be a good pick for LSTMs
 rmsprop = optimizers.RMSprop(lr=0.001)
 model.compile(loss='mean_absolute_percentage_error',
               optimizer=rmsprop)#,loss_weights={'main_output': 1., 'lepton_output': 0.33, 'hadron_output': 0.33, 'hadfrac_output': 0.33})
@@ -243,6 +211,6 @@ print(resultLog.history)
 
 #save the model definition + weights
 model.save('my_model_numu.hdf5')
-#save just the weightsx
+#save just the weights
 model.save_weights('test_numu.hdf5') 
 
