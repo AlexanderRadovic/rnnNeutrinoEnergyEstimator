e1ae4bc48a6f3f1260aa01ca6f4d81bd6d43eeda
Alexander <a.radovic@gmail.com>
2018-06-05 00:06:33 -0500

    Minor fix to a bug where I needed to create a new object.

--- weightFunctions.py
+++ weightFunctions.py
@@ -1,17 +1,20 @@
-
 #Functions to calculate sample weights.
+from __future__ import print_function
+import numpy as np
 
 #Function to pull weight from reference array
 def weightfromspectra(energy):
-    spectraweights=np.load('numuTrueSpecWeight.npy')
-    return 1000*spectraweights[int(energy*10)]
+
+    return 
 
 #Function to weight inverse to size of population in training sample
 def flatweight(y_true):
-    weights=y_true
-    print(K.shape(y_true))
-    print(K.eval(y_true))
+    spectraweights=np.load('numuTrueSpecWeight.npy')
+    weights=np.zeros(len(y_true))
     for i in range(0, len(y_true)):
-        weights[i]=weightfromspectra(y_true[i])
+        energy=int(y_true[i]*10)
+        if energy > 49:
+            energy = 49
+        weights[i]=1000*spectraweights[energy]
     
     return weights
